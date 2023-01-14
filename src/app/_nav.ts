9802e986b162e403f67ea35bbf16d2f83bcf425c
emmanuelprinceg <55761367+emmanuelprinceg@users.noreply.github.com>
2023-01-14 17:21:27 +0000

    First Commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ */
+import { INavData } from './models';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Products',
+    url: '/products',
+    icon: 'sell',
+  },
+  {
+    name: 'Orders',
+    url: '/orders',
+    icon: 'shopping_cart',
+  },
+];
